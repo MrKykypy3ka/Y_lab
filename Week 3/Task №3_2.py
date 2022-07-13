6138bb2a0234a9310d486c95129c09856bd0afb2
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2022-07-13 17:42:13 +0900

    3_2

--- Week 3/Task №3_2.py
+++ Week 3/Task №3_2.py
@@ -9,9 +9,8 @@ def wait_decorator(call_count, start_sleep_time, factor, border_sleep_time):
             print('Начало работы')
             for i in range(call_count):
                 func_result = func(*args, **kwargs)
-                if t < border_sleep_time:
-                    t = start_sleep_time * factor ** i
-                else:
+                t = start_sleep_time * factor ** i
+                if t > border_sleep_time:
                     t = border_sleep_time
                 print(f'Запуск номер {i + 1}. Ожидание: {t} секунд. Результат декорируемой функций = {func_result}.')
                 time.sleep(t)
@@ -21,7 +20,7 @@ def wait_decorator(call_count, start_sleep_time, factor, border_sleep_time):
     return decorator
 
 
-@wait_decorator(call_count=4, start_sleep_time=1, factor=2, border_sleep_time=10)
+@wait_decorator(call_count=4, start_sleep_time=2, factor=2, border_sleep_time=10)
 def multiplier(number: int):
     return number * 2
 
